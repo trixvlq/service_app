2299e5ebec71c6a32f5c75dc12b23aef4b60268e
author <qwefghnz@gmail.com>
2023-12-26 21:25:16 +0600

    changed it from calculating price in SerializerMethodField to annotate function

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -9,7 +9,9 @@ from services.serializers import SubscriptionSerializer
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().select_related('plan').prefetch_related(
-        Prefetch('client',queryset=Client.objects.all().select_related('user').only('company_name',
-                                                                                            'user__email')),
-    )
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
+                                                                                     'user__email')),
+    ).annotate(price=F('service__full_price') -
+                     (F('service__full_price') *
+                     F('plan__discount_percent')/100.00))
     serializer_class = SubscriptionSerializer
