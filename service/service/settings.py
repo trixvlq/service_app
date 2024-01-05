18c1113872afb88e22b90e1f209c65969613ffa3
author <qwefghnz@gmail.com>
2024-01-05 16:24:10 +0600

    created celery, changed response list, created total amount for an api responce, wrote my first celery worker

--- service/service/settings.py
+++ service/service/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-(n1zu7+x+w%^3a6b93#aaos=4aya#+zr&37a(+3&*8)&-p@&p+
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,13 +70,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'service.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
@@ -103,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -114,7 +110,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -127,14 +122,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGGING = {
-    'version':1,
-    'handlers':{
-        'console':{'class':'logging.StreamHandler'}
+    'version': 1,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'}
     },
-    'loggers':{
-        'django.db.backends':{
-            'handlers':['console'],
-            'level':'DEBUG'
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
         }
     }
 }
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch, F
+from django.db.models import Prefetch, F, Sum
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -13,5 +13,13 @@ class SubscriptionView(ReadOnlyModelViewSet):
                                                                                      'user__email')),
     ).annotate(price=F('service__full_price') -
                      (F('service__full_price') *
-                     F('plan__discount_percent')/100.00))
+                      F('plan__discount_percent') / 100.00))
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+        response_data = {'result': response.data}
+        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response.data = response_data
+        return response
